09ce8cca7f47b0604d0b63dc396e3235730cd25a
fs45 <fs45.tech@gmail.com>
2020-10-28 17:33:02 +0800

    documentation

---
+++ README.md
@@ -0,0 +1,14 @@
+## Install relevant packages
+`npm install -g firebase-tools`
+
+## Firebase login
+`firebase login`
+A new browser tab will be opened to allow firebase connection on local device.
+
+## Install packages for functions.
+Go to `functions` directory and run `npm install`
+
+## Deploy new functions
+In `root directory`, run
+`firebase deploy`
+
--- test.js
+++
@@ -1,8 +0,0 @@
-var http = require('http');
-var fs = require('fs');
-//create a server 
-http.createServer(function (req, res) {
-  var files = fs.createReadStream("package.json");
-  res.writeHead(200, {'Content-disposition': 'attachment; filename=package.json"}'}); //here you can specify file name
-  files.pipe(res); // also you can set content-type
-}).listen(8080); //the server object listens on port 8080
